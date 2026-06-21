d56d46dff145272a6fa8b190dd405490325ef42a
j-waweru <jwaweru@proton.me>
2026-06-21 13:00:06 +0300

    added zellij config

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -68,32 +68,83 @@ return {
     -- HARPOON
     -- =========================================
     {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" },
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
 
-        config = function()
-            require("harpoon").setup()
-        end,
+    config = function()
+        require("harpoon"):setup()
+    end,
 
-        keys = {
-            {
-                "<leader>ha",
+    keys = {
+        {
+            "<leader>ha",
+            function()
+                require("harpoon"):list():add()
+            end,
+            desc = "Harpoon add file",
+        },
+                    {
+                "<leader>hc",
                 function()
-                    require("harpoon"):list():add()
+                    require("harpoon"):list():clear()
                 end,
-                desc = "Harpoon Add",
             },
-            {
-                "<leader>hh",
-                function()
-                    local harpoon = require("harpoon")
-                    harpoon.ui:toggle_quick_menu(harpoon:list())
-                end,
-                desc = "Harpoon Menu",
-            },
+
+        {
+            "<leader>hh",
+            function()
+                local harpoon = require("harpoon")
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end,
+            desc = "Harpoon menu",
+        },
+
+        {
+            "<leader>h1",
+            function()
+                require("harpoon"):list():select(1)
+            end,
+            desc = "Harpoon file 1",
+        },
+        {
+            "<leader>h2",
+            function()
+                require("harpoon"):list():select(2)
+            end,
+            desc = "Harpoon file 2",
+        },
+        {
+            "<leader>h3",
+            function()
+                require("harpoon"):list():select(3)
+            end,
+            desc = "Harpoon file 3",
+        },
+        {
+            "<leader>h4",
+            function()
+                require("harpoon"):list():select(4)
+            end,
+            desc = "Harpoon file 4",
+        },
+
+        {
+            "<C-h>",
+            function()
+                require("harpoon"):list():prev()
+            end,
+            desc = "Harpoon prev",
+        },
+        {
+            "<C-l>",
+            function()
+                require("harpoon"):list():next()
+            end,
+            desc = "Harpoon next",
         },
     },
+},
 
     -- =========================================
     -- UNDOTREE
